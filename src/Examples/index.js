249ce09cc1b5f6373535dd6bd938c23ad62fb974
Malin J <millianapia@gmail.com>
2022-06-18 18:19:11 +0200

    SpacedNumberpicker

--- src/Examples/index.js
+++ src/Examples/index.js
@@ -1,10 +1,15 @@
 import PachinkoCheckboxes from "./PachinkoCheckboxes"
+import SpaceNumberPicker from "./SpaceNumberPicker"
 
 const AllExamples = [
     {
         name: "PachinkoCheckboxes",
         component: PachinkoCheckboxes,
-    }
+    },
+    {
+        name: "SpaceNumberPicker",
+        component: SpaceNumberPicker,
+    },
 ]
 
 export default AllExamples
